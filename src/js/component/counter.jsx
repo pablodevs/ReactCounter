b7ec715e338f1d3ec5ca9ab6fcf017116d0cb276
peibol888 <pabloalamovargas@gmail.com>
2021-11-04 12:09:14 +0000

    mejoras!

--- src/js/component/counter.jsx
+++ src/js/component/counter.jsx
@@ -5,7 +5,8 @@ import PropTypes from "prop-types";
 const SecondsCounter = props => {
 	// Hooks
 	const [count, setCount] = useState(0);
-	const [timeOn, setTimeOn] = useState(true);
+	const [timeOn, setTimeOn] = useState(false);
+	const [state, setState] = useState(true);
 
 	useEffect(() => {
 		let interval = null;
@@ -32,22 +33,42 @@ const SecondsCounter = props => {
 	return (
 		<div className="container-xl counter-div">
 			<div className="row py-2 justify-content-center">
-				<div
-					className="col px-4"
-					style={{ color: "rgb(124, 240, 255)" }}>
+				<div className="col" style={{ color: "rgb(124, 240, 255)" }}>
 					<i className="fas fa-clock"></i>
 				</div>
-				<div className="col px-4">{arrayNum[5] ? arrayNum[5] : 0}</div>
-				<div className="col px-4">{arrayNum[4] ? arrayNum[4] : 0}</div>
-				<div className="col px-4">{arrayNum[3] ? arrayNum[3] : 0}</div>
-				<div className="col px-4">{arrayNum[2] ? arrayNum[2] : 0}</div>
-				<div className="col px-4">{arrayNum[1] ? arrayNum[1] : 0}</div>
-				<div className="col px-4">{arrayNum[0]}</div>
+				<div className="col">{arrayNum[5] ? arrayNum[5] : 0}</div>
+				<div className="col">{arrayNum[4] ? arrayNum[4] : 0}</div>
+				<div className="col">{arrayNum[3] ? arrayNum[3] : 0}</div>
+				<div className="col">{arrayNum[2] ? arrayNum[2] : 0}</div>
+				<div className="col">{arrayNum[1] ? arrayNum[1] : 0}</div>
+				<div className="col">{arrayNum[0]}</div>
 			</div>
 			<div className="button--wrapper">
-				<button onClick={() => setTimeOn(true)}>Start</button>
-				<button onClick={() => setTimeOn(false)}>Stop</button>
-				<button onClick={() => setTimeOn(true)}>Resume</button>
+				<button
+					onClick={() => {
+						setTimeOn(false);
+						setCount(0);
+						setState(true);
+					}}>
+					Reset
+				</button>
+				{state ? (
+					<button
+						onClick={() => {
+							setTimeOn(true);
+							setState(false);
+						}}>
+						<i className="fas fa-play"></i>
+					</button>
+				) : (
+					<button
+						onClick={() => {
+							setTimeOn(false);
+							setState(true);
+						}}>
+						<i className="fas fa-pause"></i>
+					</button>
+				)}
 			</div>
 		</div>
 	);
